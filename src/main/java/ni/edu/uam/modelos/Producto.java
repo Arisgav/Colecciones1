c71ab099ab50a7ab2c037a2d4cac3403c9bfdf5c
Arisgav <amgavarrete@uamv.edu.ni>
2026-05-08 15:40:07 -0600

    Agregar Clase Producto

--- src/main/java/ni/edu/uam/modelos/Producto.java
+++ src/main/java/ni/edu/uam/modelos/Producto.java
@@ -1,4 +1,13 @@
 package ni.edu.uam.modelos;
 
 public class Producto {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private double precioCompra;
+    private double precioVenta;
+    private int cantidad;
+    //commit
+
 }
